95eab24f21b0f3d34a66c9e03d4c8f1944930af1
Thanh Nguyen <tnguyen@GBLONLAP0112.local>
2020-10-11 10:35:12 +0100

    Changing test suits' name and variable's name
    correct test description
    
    correct test description

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = (text) => {
-	const words = text.toLowerCase().match(/(\w'?)+/g);
-	const total = words
+	const words = text.toLowerCase().match(/[a-z']+/g);
+	const wordsCount = words
 		? words.reduce(
 				(accumulate, current) => (
 					(accumulate[current] = (accumulate[current] || 0) + 1), accumulate
@@ -8,7 +8,8 @@ module.exports = (text) => {
 				{}
 		  )
 		: [];
-	return Object.keys(total)
-		.sort((a, b) => total[b] - total[a])
+
+	return Object.keys(wordsCount)
+		.sort((a, b) => wordsCount[b] - wordsCount[a])
 		.slice(0, 3);
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -14,36 +14,36 @@ describe("Top Three Words", () => {
 		const wordsArray = chance.n(chance.word, chance.natural({ min: 1, max: 3 }));
 		const wordsString = wordsArray.join(" ");
 
-		it("should return an array with those two words", () => {
+		it("should return an array with those words", () => {
 			expect(topThreeWords(wordsString)).to.deep.equal(wordsArray);
 		});
 	});
 
-	describe("when the texts contain more than three words", () => {
-		describe("scenario 1", () => {
+	describe("when the texts contain not just words", () => {
+		describe("when texts contain line break", () => {
 			const scenario1 = `In a village of La Mancha, the name of which I have
 no desire to call to
 mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a 
 lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most nights, scraps on 
 Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, made away with three-quarters of his income.`;
 
-			it("should return expected array", () => {
+			it("should handle line breaks", () => {
 				expect(topThreeWords(scenario1)).to.deep.equal(["a", "of", "on"]);
 			});
 		});
 
-		describe("scenario 2", () => {
+		describe("when texts contain  mix of words in uppercase and lowercase", () => {
 			const scenario2 = "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e";
 
-			it("should return expected array", () => {
+			it("should match case-insensitive", () => {
 				expect(topThreeWords(scenario2)).to.deep.equal(["e", "ddd", "aa"]);
 			});
 		});
 
-		describe("scenario 3", () => {
+		describe("when texts contain apostrophes", () => {
 			const scenario3 = " //wont won't won't";
 
-			it("should return expected array", () => {
+			it("should handle words with apostrophes", () => {
 				expect(topThreeWords(scenario3)).to.deep.equal(["won't", "wont"]);
 			});
 		});
